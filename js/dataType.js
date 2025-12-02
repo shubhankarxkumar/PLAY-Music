d64241a5948335b1bbcdc00262fa5223b24ea55c
Shubhankar Kumar <shubhankarxkumar@gmail.com>
2025-12-02 20:22:05 +0530

    Read Me
    Well this is a Music page - very simple just to test some basic Skills

---
+++ js/dataType.js
@@ -0,0 +1,34 @@
+/*
+types of data type:
+number 2 ^ 53
+string ""
+boolean
+null - object - we don't have an value rn
+undefined - undefined - undefined
+*/
+
+// name = "Hiteache"
+
+// console.log(typeof "altoman")
+
+
+// let age = 21
+
+// let str = String(age)
+
+// console.log(typeof str)
+// console.log(age)
+
+// let aurth = 1
+
+// console.log(typeof aurth)
+// let bool = Boolean(aurth)
+
+// console.log(bool)
+
+let x = null
+
+let x2 = Number(x)
+
+console.log(typeof x2)
+
---
+++ js/dataType2.js
@@ -0,0 +1,28 @@
+const accountName = "Namit"
+const accountId = 21007
+const outsideTemp = null
+let accountBranch
+const BigInt = 12121212324223n
+
+const isLoggedIn = true
+
+
+const arr = ["aldorado","Omega","Oman"]
+
+const obj = {
+  "name": "oglu",
+  "artho" : "Son"
+}
+
+// console.log(typeof accountName,
+//   typeof accountId, typeof outsideTemp,
+//   typeof accountBranch,
+//   typeof BigInt,
+//   typeof isLoggedIn);
+
+
+let myfunction = function () {
+  console.log("Hell Yeah Bro");
+}
+
+myfunction();
